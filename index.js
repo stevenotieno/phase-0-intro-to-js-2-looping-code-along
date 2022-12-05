36fe0a5e74b024f1f27eb20d915344401c39b02c
+ <+>
2022-12-05 14:33:57 +0300

    comlpeted assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let writeCards = (cardNames, cardMessage) => {
+    let thankYouMessages = [];
+    for(let i = 0; i < cardNames.length; i++) {
+       thankYouMessages.push(`Thank you, ${cardNames[i]}, for the wonderful ${cardMessage} gift!`);
+    }
+    return thankYouMessages;
+ }
+ 
+ // log each number when counting down starting from the number provided to zero
+ let countDown = (i) => {
+    while (i >= 0) {
+       console.log(i);
+       i--
+    }
+ }  
